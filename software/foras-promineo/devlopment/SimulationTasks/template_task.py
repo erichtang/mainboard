debefa35d4cb4414e1983655f93cf213f15bd5d4
caden_hillis <48217362+cadenhillis@users.noreply.github.com>
2022-06-28 17:13:04 -0400

    documentation

---
+++ software/foras-promineo/devlopment/SimulationTasks/template_task.py
@@ -0,0 +1,81 @@
+# SIMULATION!!!
+from debugcolor import co
+import config
+
+class Task:
+
+    """
+    The Task Object.
+
+    Attributes:
+        priority:    The priority level assigned to the task.
+        frequency:   Number of times the task must be executed in 1 second (Hz).
+        name:        Name of the task object for future reference
+        color:       Debug color for serial terminal
+    """
+
+    priority = 10
+    frequency = 1
+    name = 'template'
+    color = 'gray'
+
+    def __init__(self, satellite):
+        """
+        Initialize the Task using the PyCubed cubesat object.
+        
+        :type satellite: Satellite
+        :param satellite: The cubesat to be registered
+
+        """
+        self.cubesat = satellite
+        
+    def debug(self,msg,level=1,log=False):
+        """
+        Print a debug message formatted with the task name and color, can append logfile
+
+        :param msg: Debug message to print
+        :param level: > 1 will print as a sub-level
+        :param log: appends logfile if True #added CH
+        """
+        if level==1:
+            print('{:>30} {}'.format('['+co(msg=self.name,color=self.color)+']',msg))
+        else:
+            print('{}{}'.format('\t   └── ',msg))
+        if log:
+            self.cubesat.log(msg, print_flag = False)
+
+    async def main_task(self, *args, **kwargs):
+        """
+        Contains the code for the user defined task. 
+
+        :param `*args`: Variable number of arguments used for task execution.
+        :param `**kwargs`: Variable number of keyword arguments used for task execution. 
+
+        """
+        pass
+    
+    def load_cfg(self, cfg, name, sd=False):
+        #loading of different named configs for different operating modes is possible by passing differnet names
+        #load prev prio and freq if they exist
+        try:
+            prev_prio = cfg['priority']
+            prev_freq = cfg['frequency']
+        except:
+            prev_prio = -1
+            prev_freq = -1
+        #update config
+        config.load_cfg(cfg, name, self.cubesat.log, sd)
+        #if new prio and freq we update the self.cubesat.scheduled tasks attribute
+        try:
+            if cfg['priority'] != prev_prio:
+                self.cubesat.scheduled_tasks[self.name].change_priority(cfg['priority'])
+            if cfg['frequency'] != prev_freq:
+                self.cubesat.scheduled_tasks[self.name].change_rate(cfg['frequency'])
+        except:
+            self.cubesat.log("[ERROR][PRIORITY / FREQ UPDATE][{}][FAIL]".format(name))
+
+    def save_cfg(self, cfg, name, sd=False):
+        #literally the same for save cfg i think.
+        config.save_config(cfg, name, self.cubesat.log, sd)
+
+    
--- software/foras-promineo/devlopment/db_cdh.py
+++ software/foras-promineo/devlopment/db_cdh.py
@@ -4,9 +4,22 @@ from debugcolor import co
 
 ########### commands without arguments ###########
 def noop(self):
+    """
+    A no-op command.
+
+    Returns:
+        "ACK"
+    """
     write(co('ACK', 'green'))
 
 def hreset(self):
+    """
+    Hard resets the MCU. (Powers off and on again)
+
+    Returns:
+        Success: Board initalization prints on both terminals
+        Failure: "Reset Failed"
+    """
     write('Resetting')
     try:
         self.cubesat.micro.on_next_reset(self.cubesat.micro.RunMode.NORMAL)
@@ -14,13 +27,64 @@ def hreset(self):
     except:
         write('Reset Failed')
 
+def sreset(self):
+    """
+    TODO
+    Soft reset of the MCU (no power cycle)
+
+    Returns:
+        Success: Board initalization prints on both terminals
+        Failure: "Reset Failed"
+    """
+
+def i2c_scan(self):
+    """
+    TODO
+    i2c device scan
+
+    Returns:
+        detected i2c devices
+    """
+    while not self.cubesat.i2c1.try_lock():
+        pass
+    try:
+        print(
+            "I2C addresses found:",
+            [hex(device_address) for device_address in self.cubesat.i2c1.scan()],
+        )
+    except Exception as e:
+        write('Execution failed... : {}'.format(e))
+
+def sd_ls(self):
+    pass
+
+def download_log(self):
+    pass
+
 ########### commands with arguments ###########
 
 def query(self,args):
+    """
+    runs python method eval(args)
+
+    Returns:
+        Success: results of eval
+        Failure: exeception / error
+    """
     write('query: {}'.format(args))
-    write(eval(bytes(args,'utf-8')))
+    try:
+        write(eval(bytes(args,'utf-8')))
+    except Exception as e:
+        write('Evaluation failed... : {}'.format(e))
 
 def exec_cmd(self,args):
+    """
+    runs python method eval(args)
+
+    Returns:
+        Success: nothing 
+        Failure: exeception / error
+    """
     arg = args[0]
     write('exec: {}'.format(arg))
     try:
@@ -28,183 +92,85 @@ def exec_cmd(self,args):
     except Exception as e:
         write('Execution failed... : {}'.format(e))
 
-def get_imu_offset(self, args):
+def stop_task(self, task): 
     """
-    records RAW offset's of imu paramaters.
-    write currently inimplemented (allowing automatic compensation of calculated offsets when it is)
-    args[0] = x : # of measurements taken and averaged
-    args[1] (default False) = wr : when True writes offsets to calibration dict. doesn't have to be passed.
-    """
-    #how to get offset with magnetometer?
-    #add tabple entries for PIB
-    polling_dict = {
-        'gyro0': self.cubesat.imu.gyro0_r_raw,
-        'gyro1': self.cubesat.imu.gyro1_r_raw,
-        'mag0' : self.cubesat.imu.mag0_r_raw,
-        'mag1' : self.cubesat.imu.mag1_r_raw,
-        'accel0': self.cubesat.imu.accel0_r_raw,
-        'accel1': self.cubesat.imu.accel1_r_raw,
-    }
-    #args extraction
-    x = int(args[0])
-    if len(args)==2:
-        wr = args[1]
-    else:
-        wr = False
-    #execution of function
-    for device in polling_dict:
-        #write(device)
-        i = 0
-        temp_avg = [0,0,0]
-        while i < x :
-            #get measurements
-            temp = polling_dict[device]
-            if temp is None:
-                write("Device {} returned None on i = {} out of {}".format(device, i, x))
-                temp_avg = None
-                break
-            else:
-                for meas in range(len(temp_avg)):
-                    temp_avg[meas] =+ temp[meas]
-            time.sleep(0.01)
-            i+=1
-        #average
-        if temp_avg is not None:
-            for num in range(len(temp_avg)):
-                temp_avg[num] = int(temp_avg[num]/x) #whole numbers, factional offsets would not make sense for a raw value.
-            #print 
-            write("Device {} Offset Calculation for {} readings: ".format(device, x) + co('{}'.format(temp_avg), 'green', 'bold'))
+    TODO 
+    stops task
 
-def startstoptask(self, args):
-    """
-    starts or stops specified task.
-    args[0] = task name
-    args[1] = "st" or "sp"
+    Paramaters:
+        task :: the task key in cubesat.scheduled_tasks dict
+
+    Returns:
+        None
     """
     pass
 
-def i2c_scan(self):
-    while not self.cubesat.i2c1.try_lock():
-        pass
-    try:
-        while True:
-            print(
-                "I2C addresses found:",
-                [hex(device_address) for device_address in self.cubesat.i2c1.scan()],
-            )
-            time.sleep(2)
-    finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
-       self.cubesat.i2c1.unlock()
-
-def pib_verify(self):
+def start_task(self, task): 
     """
-    PIB checkout function to verify functionality on the PIB
+    TODO 
+    starts task
+
+    Paramaters:
+        task :: the task key in cubesat.scheduled_tasks dict
+
+    Returns:
+        None
     """
-    # verify imu
-    write("verifing imu ....")
-    write("Gyro 0 : {}".format(self.cubesat.pib.imu.gyro0))
-    write("Gyro 1 : {}".format(self.cubesat.pib.imu.gyro1))
-    write("Mag 0 : {}".format(self.cubesat.pib.imu.mag0))
-    write("Mag 1 : {}".format(self.cubesat.pib.imu.mag1))
-    write("Accel 0 : {}".format(self.cubesat.pib.imu.accel0))
-    write("Accel 1 : {}".format(self.cubesat.pib.imu.accel1))
-    write("")
+    pass
 
+def change_priority(self, task):
+    """
+    TODO
+    changes task priority
 
-    # verify io exp
-    write("verifying io_exp... turning all outputs HIGH except SLP lines")
-    self.cubesat.pib.n_dac_lat0 = True
-    self.cubesat.pib.n_dac_lat0 = True
-    self.cubesat.pib.n_amp_shdn_xy = False
-    self.cubesat.pib.n_amp_shdn_z = False
-    self.cubesat.pib.drv_ph_x = True
-    self.cubesat.pib.drv_en_x = True
-    self.cubesat.pib.n_drv_slp_x = False
-    self.cubesat.pib.drv_ph_y = True
-    self.cubesat.pib.drv_en_y = True
-    self.cubesat.pib.n_drv_slp_y = False
-    self.cubesat.pib.drv_ph_z = True
-    self.cubesat.pib.drv_en_z = True
-    self.cubesat.pib.n_drv_slp_z = False
-    self.cubesat.pib.n_dac_rst = True
-    write("")
-    waitforinput()
+    Paramaters:
+        task :: the task key in cubesat.scheduled_tasks dict
+    
+    Returns:
+        None
+    """
+    pass
 
-    # verify dac
-    write("verifying dac")
-    write("setting all outputs to 1.25V")
-    self.cubesat.pib.dac.dac0 = 2^6
-    self.cubesat.pib.dac.dac1 = 2^6
-    self.cubesat.pib.dac.dac2 = 2^6
-    write("")
-    waitforinput()
+def change_frequency(self, task):
+    """
+    TODO
+    changes task frequency
 
-    # verify amp
-    write("verifing amp")
-    write("turning all outputs ON, output should be ~2.5V")
-    self.cubesat.pib.n_amp_shdn_xy = True
-    self.cubesat.pib.n_amp_shdn_z = True
-    write("")
-    waitforinput()
+    Paramaters:
+        task :: the task key in cubesat.scheduled_tasks dict
+    
+    Returns:
+        None
+    """
+    pass
 
-    # verify h-bridge
-    write("verifing h-bridge")
-    write("turning all outputs ON in + direction")
-    self.cubesat.pib.drv_ph_x = True
-    self.cubesat.pib.drv_ph_y = True
-    self.cubesat.pib.drv_ph_z = True
-    self.cubesat.pib.drv_en_x = True
-    self.cubesat.pib.drv_en_y = True
-    self.cubesat.pib.drv_en_z = True
-    self.cubesat.pib.n_drv_slp_x = True
-    self.cubesat.pib.n_drv_slp_y = True
-    self.cubesat.pib.n_drv_slp_z = True
-    write("")
-    waitforinput()
+def configure_task(self, task, *kwargs):
+    pass
 
-    # verify i sen
-    write("verifing sense line")
-    write("sense resistor is not installed, so readings should be railed")
-    self.cubesat.pib.adc.ch = 0
-    write("CH0: {}".format(self.cubesat.pib.adc.read))
-    time.sleep(0.01)
-    write("CH0: {}".format(self.cubesat.pib.adc.read))
-    self.cubesat.pib.adc.ch = 1
-    write("CH1: {}".format(self.cubesat.pib.adc.read))
-    time.sleep(0.01)
-    write("CH1: {}".format(self.cubesat.pib.adc.read))
-    self.cubesat.pib.adc.ch = 2
-    write("CH2: {}".format(self.cubesat.pib.adc.read))
-    time.sleep(0.01)
-    write("CH2: {}".format(self.cubesat.pib.adc.read))
-    self.cubesat.pib.adc.ch = 0
-    write("")
-    waitforinput()
-       
-    # verify rockblock
-    write("rockblock verification not written")
+def upload_to_sd(self, args):
+    pass
 
-    write("")
-    write("returning things to idle")
-    self.cubesat.pib.dac.dac0 = 0
-    self.cubesat.pib.dac.dac1 = 0
-    self.cubesat.pib.dac.dac2 = 0
-    self.cubesat.pib.n_amp_shdn_xy = False
-    self.cubesat.pib.n_amp_shdn_z = False
-    self.cubesat.pib.drv_ph_x = False
-    self.cubesat.pib.drv_ph_y = False
-    self.cubesat.pib.drv_ph_z = False
-    self.cubesat.pib.drv_en_x = False
-    self.cubesat.pib.drv_en_y = False
-    self.cubesat.pib.drv_en_z = False
-    self.cubesat.pib.n_drv_slp_x = False
-    self.cubesat.pib.n_drv_slp_y = False
-    self.cubesat.pib.n_drv_slp_z = False
-    write(" PIB checkout END")
-    write("")
+def download_from_sd(self, args):
+    pass
+
+def sd_rm(self, file):
+    pass
+
+def pl_cmd_arm(self, *args):
+    pass
+
+def pl_cmd_photo(self):
+    pass
+
+def img_burst(self, args):
+    pass
+
+def pl_cmd(self, cmd, *args):
+    pass
 
 def simulate(self, *args):
     """
+    TODO ignore this while i work out simualation kinks
     simulate command. 
     passed args are keys in the self.cubesat.sim.simulate dict
     they TOGGLE simulation
@@ -223,7 +189,6 @@ def write(msg):
     """
     msg = msg + '\r\n'
     usb_cdc.data.write(bytes(msg, 'utf-8'))
-
 
 def waitforinput():
     """
